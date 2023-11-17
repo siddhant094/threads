2aa6162fdbb2dc85e812e3e1319e461dcc589cc6
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-11-17 10:50:11 +0530

    commit

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,18 @@
+function RightSidebar() {
+    return (
+        <section className='custom-scroolbar rightsidebar'>
+            <div className='flex flex-1 flrx-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>
+                    Suggested Cummunitites
+                </h3>
+            </div>
+            <div className='flex flex-1 flrx-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>
+                    Suggested Users
+                </h3>
+            </div>
+        </section>
+    );
+}
+
+export default RightSidebar;
